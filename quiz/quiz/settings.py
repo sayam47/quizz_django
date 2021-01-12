2b6995ba676800baf6b9e802ed5f50845467b327
Sayam Choudhary <34365122+sayam47@users.noreply.github.com>
2021-01-12 19:15:37 +0530

    remove secret key

--- quiz/quiz/settings.py
+++ quiz/quiz/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'l-rm&22=61bal91rt&nl$r6&3@(6#7x)0q=uia(tspa@(cm5ln'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
